479402b213b74faad5bb1136b20365c2d66ec88c
USMP-lancer <42185819+USMP-lancer@users.noreply.github.com>
2021-08-17 09:04:10 -0500

    a

--- scripts/features/v3.js
+++ scripts/features/v3.js
@@ -39,7 +39,7 @@ module.exports = (add) => {
             if(e.minfo.mod) return;
             
             if(!(e instanceof Turret)) return;
-            e.reloadTime = 1;
+            e.reloadTime = 0.1;
             e.spread = 0;
             e.inaccuracy = 0;
             e.recoilAmount = 0;
